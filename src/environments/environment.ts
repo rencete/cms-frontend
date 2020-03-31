9e08bbfc64ee86311f2a8c72594ec69d6ac779bc
Terence Yu <rencete.workonline@gmail.com>
2020-03-31 16:28:45 +0800

    Add json server as Api URL to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // API Server configuration
+  // Expects json-server to be running
+  apiUrlSettings: {
+    protocol: "http",
+    url: "localhost",
+    port: 3000,
+    basePath: ""
+  }
 };
 
 /*
