ffa06818b79978a84280bc1c0dfe48ba448d95f0
Terence Yu <rencete.workonline@gmail.com>
2020-04-01 12:03:29 +0800

    Import HttpClientModule to CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { environment } from "@environments/environment";
 import { API_URL_TOKEN } from "./services/api-url.token";
@@ -7,7 +8,8 @@ import { API_URL_TOKEN } from "./services/api-url.token";
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   providers: [
     { provide: API_URL_TOKEN, useValue: () => environment.apiUrlParts }
