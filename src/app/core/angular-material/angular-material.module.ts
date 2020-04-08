ea8fe3d6df2e7bde163296c03ff4e7c00ef64853
Terence Yu <rencete.workonline@gmail.com>
2020-04-08 19:58:27 +0800

    Generate base template for webpage

--- src/app/core/angular-material/angular-material.module.ts
+++ src/app/core/angular-material/angular-material.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 const materialModules: any = [
     MatInputModule,
     MatFormFieldModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
 ]
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,15 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { environment } from "@environments/environment";
 import { API_URL_TOKEN } from "./services/api-url.token";
+import { BaseTemplateComponent } from './base-template/base-template.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { AngularMaterialModule } from '@core/angular-material/angular-material.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BaseTemplateComponent
+  ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    LayoutModule,
+    AngularMaterialModule
   ],
   providers: [
     { provide: API_URL_TOKEN, useValue: environment.apiUrlParts }
+  ],
+  exports: [
+    BaseTemplateComponent
   ]
 })
 export class CoreModule { }
