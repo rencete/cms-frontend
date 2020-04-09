bfe9d922cb41a65d6fb6e0f90e323972366060a7
Terence Yu <rencete.workonline@gmail.com>
2020-04-09 13:54:48 +0800

    Add APIs to error display model Add toString() Add isRead() add markAsRead()

--- src/app/core/models/error-display/error-display-model.ts
+++ src/app/core/models/error-display/error-display-model.ts
@@ -3,6 +3,19 @@ export class ErrorDisplayModel {
     private error: Error;
 
     constructor(err: Error) {
+        this.read = false;
         this.error = err;
+    }
+
+    toString(): string {
+        return this.error.toString();
+    }
+
+    isRead(): boolean {
+        return this.read;
+    }
+
+    markAsRead(): void {
+        this.read = true;
     }
 }
