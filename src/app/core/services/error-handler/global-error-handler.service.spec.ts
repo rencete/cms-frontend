cce3808bb600fa250cf8b9a937460008dec27499
Terence Yu <rencete.workonline@gmail.com>
2020-04-10 11:31:16 +0800

    Refactor Global Error Handler tests

--- src/app/core/services/error-handler/global-error-handler.service.spec.ts
+++ src/app/core/services/error-handler/global-error-handler.service.spec.ts
@@ -2,28 +2,56 @@ import { TestBed } from '@angular/core/testing';
 
 import { GlobalErrorHandlerService } from './global-error-handler.service';
 import { ErrorDisplayService } from "@core/services/error-display/error-display.service";
+import { RemoteLoggingService } from "@core/services/remote-logging/remote-logging.service";
+import { Observable, of } from 'rxjs';
 
 describe('GlobalErrorHandlerService', () => {
   let service: GlobalErrorHandlerService;
   let testError: Error;
   let mockErrorDisplayService: ErrorDisplayService;
+  let mockRemoteLoggingService: RemoteLoggingService;
 
   beforeEach(() => {
-    const displayService = jasmine.createSpyObj("ErrorDisplayService", ["addErrorToDisplay"]);
+    testError = new Error("test");
+    const displayService = createMockDisplayService();
+    const loggingService = createMockRemoteLoggingService(of(testError));
+    prepareTestBed(displayService, loggingService);
+  });
 
+  function createMockDisplayService(): { addErrorToDisplay: jasmine.Spy } {
+    let mockDisplayService = jasmine.createSpyObj("ErrorDisplayService", ["addErrorToDisplay"]);
+    mockDisplayService.addErrorToDisplay.and.callFake((err) => {
+      expect(err).toEqual(testError);
+    });
+    return mockDisplayService;
+  }
+
+  function createMockRemoteLoggingService(returnObs: Observable<Error>): { remoteLog: jasmine.Spy } {
+    let mockLoggingService = jasmine.createSpyObj("RemoteLoggingService", ["remoteLog"]);
+    mockLoggingService.remoteLog.and.callFake((err) => {
+      expect(err).toEqual(testError);
+      return returnObs;
+    });
+    return mockLoggingService;
+  }
+
+  function prepareTestBed(
+    mockDisplayService: { addErrorToDisplay: jasmine.Spy<InferableFunction>; },
+    mockLoggingService: { remoteLog: jasmine.Spy<InferableFunction>; }
+  ) {
     TestBed.configureTestingModule({
       providers: [
-        {provide: ErrorDisplayService, useValue: displayService}
+        { provide: ErrorDisplayService, useValue: mockDisplayService },
+        { provide: RemoteLoggingService, useValue: mockLoggingService }
       ]
     });
     service = TestBed.inject(GlobalErrorHandlerService);
-
-    testError = new Error("test");
-  });
+  }
 
   beforeEach(() => {
     spyOn(console, "error");
     mockErrorDisplayService = TestBed.inject<ErrorDisplayService>(ErrorDisplayService);
+    mockRemoteLoggingService = TestBed.inject<RemoteLoggingService>(RemoteLoggingService);
   });
 
   it('should be created', () => {
@@ -40,5 +68,11 @@ describe('GlobalErrorHandlerService', () => {
     service.handleError(testError);
 
     expect(mockErrorDisplayService.addErrorToDisplay).toHaveBeenCalled();
+  });
+
+  it('should send the error to remote logging', () => {
+    service.handleError(testError);
+
+    expect(mockRemoteLoggingService.remoteLog).toHaveBeenCalled();
   });
 });
--- src/app/core/services/error-handler/global-error-handler.service.ts
+++ src/app/core/services/error-handler/global-error-handler.service.ts
@@ -1,17 +1,31 @@
 import { Injectable, ErrorHandler, Injector } from '@angular/core';
 
 import { ErrorDisplayService } from '@core/services/error-display/error-display.service';
+import { RemoteLoggingService } from "@core/services/remote-logging/remote-logging.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalErrorHandlerService implements ErrorHandler {
-  constructor(private injector: Injector) {    
-  }
+  constructor(private injector: Injector) { }
 
   handleError(error: any): void {
     console.error(error);
+    this.sendErrorToDisplayService(error);
+    this.sendErrorToRemoteLoggingService(error);
+  }
 
+  private sendErrorToRemoteLoggingService(error: any) {
+    const remoteLogginService = this.injector.get(RemoteLoggingService);
+    remoteLogginService.remoteLog(error).subscribe(
+      () => { },
+      (err) => { 
+        console.error(err);
+        this.sendErrorToDisplayService(err);
+      });
+  }
+
+  private sendErrorToDisplayService(error: any) {
     const errorDisplayService = this.injector.get(ErrorDisplayService);
     errorDisplayService.addErrorToDisplay(error);
   }
