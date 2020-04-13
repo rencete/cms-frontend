a51a529e15681ddcb424a10f312fbbc27880f151
Terence Yu <rencete.workonline@gmail.com>
2020-04-13 09:07:23 +0800

    Add observable to error display service Observable emits when a new error is added. Value is the new error.

--- src/app/core/services/error-display/error-display.service.spec.ts
+++ src/app/core/services/error-display/error-display.service.spec.ts
@@ -33,12 +33,51 @@ describe('ErrorDisplayService', () => {
     expect(service.getUnreadErrors().length).toBe(1);
   });
 
+  it("adding multiple errors, adds them to the list of retrieved errors", () => {
+    const testError1 = new Error("test1");
+    const testError2 = new Error("test2");
+    const testError3 = new Error("test3");
+    service.addErrorToDisplay(testError1);
+    service.addErrorToDisplay(testError2);
+    service.addErrorToDisplay(testError3);
+
+    expect(service.getUnreadErrors().length).toBe(3);
+  });
+
   it("marking an error as read, removes it from the list of retrieved errors", () => {
     const testError = new Error("test");
     service.addErrorToDisplay(testError);
-    const retrievedErrorModel = service.getUnreadErrors().shift();
+    const retrievedErrorModel = service.getUnreadErrors()[0];
     retrievedErrorModel.markAsRead();
 
     expect(service.getUnreadErrors().length).toBe(0);
   });
-});
+
+  it("adding multiple errors, adds them to the list of retrieved errors", () => {
+    const testError1 = new Error("test1");
+    const testError2 = new Error("test2");
+    const testError3 = new Error("test3");
+    service.addErrorToDisplay(testError1);
+    service.addErrorToDisplay(testError2);
+    service.addErrorToDisplay(testError3);
+    
+    const retrievedErrorModel = service.getUnreadErrors()[0];
+    retrievedErrorModel.markAsRead();
+
+    expect(service.getUnreadErrors().length).toBe(2);
+  });
+
+  it("emits observable, when new error is added", () => {
+    let timesCalled: number = 0;
+    service.newErrorAdded$.subscribe(
+      () => { timesCalled += 1; },
+      (err) => { fail(); }
+    );
+    expect(timesCalled).toBe(0);
+
+    const testError = new Error("test");
+    service.addErrorToDisplay(testError);
+
+    expect(timesCalled).toBe(1);
+  });
+})
--- src/app/core/services/error-display/error-display.service.ts
+++ src/app/core/services/error-display/error-display.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
 import { ErrorDisplayModel } from '@app/core/models/error-display/error-display-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorDisplayService {
-  private errorsToBeDisplayed: ErrorDisplayModel[];
+  public newErrorAdded$: Observable<ErrorDisplayModel>;
+  private newErrorAdded: Subject<ErrorDisplayModel>;
+  private errorsToBeDisplayed: ErrorDisplayModel[];  
 
   constructor() {
+    this.newErrorAdded = new Subject<ErrorDisplayModel>();
+    this.newErrorAdded$ = this.newErrorAdded.asObservable();
     this.errorsToBeDisplayed = [];
   }
 
   addErrorToDisplay(err: Error): void {
-    this.errorsToBeDisplayed.push(new ErrorDisplayModel(err));
+    const newError = new ErrorDisplayModel(err);
+    this.errorsToBeDisplayed.push(newError);
+    this.newErrorAdded.next(newError);
   }
 
   getUnreadErrors(): ErrorDisplayModel[] {
