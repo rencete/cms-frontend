e1212027ecbb1e4110a9eff78e056f214cab1e0f
Terence Yu <rencete.workonline@gmail.com>
2020-04-13 09:15:37 +0800

    Add cli generated error display component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -9,10 +9,12 @@ import { API_URL_TOKEN } from "./services/api-url.token";
 import { BaseTemplateComponent } from './base-template/base-template.component';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { GlobalErrorHandlerService } from './services/error-handler/global-error-handler.service';
+import { ErrorDisplayComponent } from './components/error-display/error-display.component';
 
 @NgModule({
   declarations: [
-    BaseTemplateComponent
+    BaseTemplateComponent,
+    ErrorDisplayComponent
   ],
   imports: [
     BrowserModule,
