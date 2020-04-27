4f6d523be814ff50d8308451aab1b9c54ef6cdec
Terence Yu <rencete.workonline@gmail.com>
2020-04-27 16:37:09 +0800

    Add logic to ErrorFacade to send to RemoteLogger

--- src/app/error/services/error-facade.service.spec.ts
+++ src/app/error/services/error-facade.service.spec.ts
@@ -3,13 +3,19 @@ import { Observable } from 'rxjs';
 
 import { ErrorFacade } from './error-facade.service';
 import { ErrorStoreService } from './error-store.service';
-import { ErrorData } from '../models/error-data.model';
+import { ErrorData } from '@app/error/models/error-data.model';
+import { RemoteLoggingService } from '@app/core/services/remote-logging/remote-logging.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('ErrorFacade', () => {
   let service: ErrorFacade;
   let mockStore: {
     unreadErrors$: Observable<ErrorData[]>;
+    addError: jasmine.Spy;
     markAsRead: jasmine.Spy;
+  };
+  let mockRemoteLog: {
+    remoteLog: jasmine.Spy;
   };
   const testErrorData: ErrorData[] = [
     { id: "1", name: "Error 1", message: "This is sample error 1" },
@@ -18,18 +24,26 @@ describe('ErrorFacade', () => {
   ];
 
   beforeEach(() => {
-    let errorStoreMock = jasmine.createSpyObj("ErrorStoreService", ["markAsRead"]);
-    errorStoreMock.unreadErrors$ = new Observable((subcriber) => {      
+    let errorStoreMock = jasmine.createSpyObj("ErrorStoreService", ["addError", "markAsRead"]);
+    errorStoreMock.unreadErrors$ = new Observable((subcriber) => {
       subcriber.next(testErrorData);
     })
+    let remoteLogMock = jasmine.createSpyObj("RemoteLoggingService", ["remoteLog"]);
+    remoteLogMock.remoteLog.and.callFake(() => {
+      return new Observable<any>((s) => {
+        s.complete();
+      })
+    });
 
     TestBed.configureTestingModule({
       providers: [
-        { provide: ErrorStoreService, useValue: errorStoreMock }
+        { provide: ErrorStoreService, useValue: errorStoreMock },
+        { provide: RemoteLoggingService, useValue: remoteLogMock }
       ]
     });
     service = TestBed.inject(ErrorFacade);
     mockStore = TestBed.get(ErrorStoreService);
+    mockRemoteLog = TestBed.get(RemoteLoggingService);
   });
 
   it('should get list of unread errors as observable', (done) => {
@@ -46,5 +60,34 @@ describe('ErrorFacade', () => {
     const testId = "test-id";
     service.markErrorAsRead(testId);
     expect(mockStore.markAsRead).toHaveBeenCalledWith(testId);
+  });
+
+  it('should pass Error to Store to add errors', () => {
+    const testError = new Error("This is a test error");
+    service.handleError(testError);
+    expect(mockStore.addError).toHaveBeenCalledWith(testError);
+  });
+
+  it('should send the error to remote logging', () => {
+    const testError = new Error("This is a test error");
+    service.handleError(testError);
+
+    expect(mockRemoteLog.remoteLog).toHaveBeenCalledTimes(1);
+  });
+
+  it('should send error response from remote logging to error store', () => {
+    const errorResponse = new HttpErrorResponse({ status: 404, statusText: "Test Error" })
+    mockRemoteLog.remoteLog.and.callFake(() => {
+      console.log("here");
+      return new Observable<any>((s) => {
+        console.log(s);
+        s.error(errorResponse);
+      });
+    });
+    const testError = new Error("This is a test error");
+    service.handleError(testError);
+
+    expect(mockStore.addError).toHaveBeenCalledTimes(2);
+    expect(mockStore.addError).toHaveBeenCalledWith(errorResponse);
   });
 });
--- src/app/error/services/error-facade.service.ts
+++ src/app/error/services/error-facade.service.ts
@@ -3,12 +3,17 @@ import { Injectable } from '@angular/core';
 import { ErrorStoreService } from './error-store.service';
 import { Observable } from 'rxjs';
 import { ErrorData } from '../models/error-data.model';
+import { ErrorModelInterface } from '../models/error-model.interface';
+import { RemoteLoggingService } from '@app/core/services/remote-logging/remote-logging.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorFacade {
-  constructor(private store: ErrorStoreService) { }
+  constructor(
+    private store: ErrorStoreService,
+    private remoteLogger: RemoteLoggingService
+  ) { }
 
   getUnreadErrors(): Observable<ErrorData[]> {
     return this.store.unreadErrors$;
@@ -16,5 +21,25 @@ export class ErrorFacade {
 
   markErrorAsRead(id: string): void {
     this.store.markAsRead(id);
+  }
+
+  handleError(err: ErrorModelInterface): void {
+    this.addToStore(err);
+    this.sendToRemoteLog(err);
+  }
+
+  private addToStore(err: ErrorModelInterface) {
+    this.store.addError(err);
+  }
+
+  private sendToRemoteLog(err: ErrorModelInterface) {
+    let obs = this.remoteLogger.remoteLog(err)
+    obs.subscribe(
+      () => { },
+      (errorResponse) => {
+        console.log(errorResponse);
+        this.addToStore(errorResponse);
+      }
+    );
   }
 }
