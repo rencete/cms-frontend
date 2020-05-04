23db9357ba56c631e0190b9d5e95b5dc2ec8ed41
Terence Yu <rencete.workonline@gmail.com>
2020-05-04 22:55:25 +0800

    Fix for warning in console for BaseTemplate test Prevents warnings of the ff: 'router-outlet' is not a known element

--- src/app/core/base-template/base-template.component.spec.ts
+++ src/app/core/base-template/base-template.component.spec.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterTestingModule } from "@angular/router/testing";
 
 import { BaseTemplateComponent } from './base-template.component';
 
@@ -17,6 +18,7 @@ describe('BaseTemplateComponent', () => {
     TestBed.configureTestingModule({
       declarations: [BaseTemplateComponent],
       imports: [
+        RouterTestingModule,
         NoopAnimationsModule,
         LayoutModule,
         MatButtonModule,
